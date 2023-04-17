eb2d67244ae9f4a2fbcf5f9df87c34be678d1ec4
Samantha Martinez <samanthamartinez8290@gmail.com>
2023-04-17 15:41:48 -0500

    submit

--- frontend-vue/src/router/index.js
+++ frontend-vue/src/router/index.js
@@ -5,6 +5,7 @@ import Login from '../views/Login.vue'
 import Logout from '../views/Logout.vue'
 import Register from '../views/Register.vue'
 import store from '../store/index'
+import LandingPage from '../views/LandingPage.vue'
 
 Vue.use(Router)
 
@@ -27,6 +28,14 @@ const router = new Router({
       component: Home,
       meta: {
         requiresAuth: true
+      }
+    },
+    {
+      path: '/landing',
+      name: 'landing-page',
+      component: LandingPage,
+      meta: {
+        requiresAuth: false
       }
     },
     {
