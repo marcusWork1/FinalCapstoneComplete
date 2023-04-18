d3fb7885c4b641683f9cd854a8f16484936b65dd
Matthew Grecco <greccomatthew269@gmail.com>
2023-04-18 13:20:25 -0400

    finished path

--- backend-server/src/main/java/com/techelevator/controller/capstone/FavoriteListController.java
+++ backend-server/src/main/java/com/techelevator/controller/capstone/FavoriteListController.java
@@ -1,7 +1,9 @@
 package com.techelevator.controller.capstone;
 
 import com.techelevator.dao.FavoriteListDAO;
+import com.techelevator.dao.MovieDAO;
 import com.techelevator.model.FavoriteList;
+import com.techelevator.model.Movie;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,17 @@ import java.util.List;
 @RestController
 public class FavoriteListController {
 
+    private FavoriteListDAO favoriteListDAO;
+
+    public FavoriteListController(FavoriteListDAO aFavoriteDao) {
+        this.favoriteListDAO = aFavoriteDao;
+    }
+
     // Need a post to handle the path from Vue database service with status created
-    // use the create method that laura creates 
+    // use the create method that laura creates
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(path="/favorite" , method = RequestMethod.POST)
+    public FavoriteList favorite(@Valid @RequestBody FavoriteList favMovie) {
+        return favoriteListDAO.addFavorite(favMovie);
+    }
 }
