feb0496f1c7d6b815bd1e1822b2d1ccfbfd66762
Laura Phung <sincerelylau@gmail.com>
2023-04-18 10:45:17 -0400

    Access to OMDB API

---
+++ frontend-vue/src/services/OMDBService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const APIServer = axios.create({
+  baseURL: "http://www.omdbapi.com",
+  //apiKEY: "apikey=ea9877be",
+});
+
+export default {
+  getMovies() {//right now it's just get Godfather the movie
+    //need to figure out how to do random movies or all movies??
+    return APIServer.get("/?s=Godfather&apikey=ea9877be").then((response) => {
+      return response.data.cards;
+    });
+  },
+
+  getMovieByMovieId(movieId) {
+    return APIServer.get("/?i=${movieId}&apikey=ea9877be");
+  },
+}; //end of export
