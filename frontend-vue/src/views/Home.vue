cc69e98c6a0ce0468f6d1b8cbb31f2aeaa408b77
Samantha Martinez <samanthamartinez8290@gmail.com>
2023-04-18 19:03:42 -0500

    submit

--- frontend-vue/src/views/Home.vue
+++ frontend-vue/src/views/Home.vue
@@ -3,24 +3,11 @@
     <body id = "body">
             <header>
             <a href="http://localhost:8080/"><img class = "imgicon" src="https://i.imgur.com/8wIaQDQ.png" alt="webicon"/> </a>
-            <a href="#"><img class = "descicon" src="https://i.imgur.com/ZfFZoTz.jpg" alt="descicon"/> </a> 
+            <a href="http://localhost:8080/description"><img class = "descicon" src="https://i.imgur.com/ZfFZoTz.jpg" alt="descicon"/> </a> 
             <a href="#"><img class = "contacticon" src="https://i.imgur.com/WtWooFI.jpg" alt="contacticon"/> </a> 
             <a href="http://localhost:8080/login"><img class = "logicon" src="https://i.imgur.com/kSKskRm.png" alt="logicon"> </a>
         </header>
     <navigate />
- 
-    <div>
-    <p>Welcome to DateFlix and Chill ;)</p>
-    <h2>About Dateflix and Chill</h2>
-    <p>In April 2023, six Tech Elevator Programmers were tired of 
-      bad movie recommendations that did not align with their interest.
-      Together they brainstromed and created an app that comes up with the best
-      movie to watch based on your personal preference and mood when trying to find
-      the perfect title to watch for date night. Our app follows a tinder-style 
-      format of clicking right or left on a movie if you’ve seen it, and you rate how you felt about 
-      it the app would suggest movies based off of your swipes. 
-    </p>
-   </div>
     <h2><router-link to =/login> Login or Sign Up Here! </router-link></h2>
     </body>
   </div>
