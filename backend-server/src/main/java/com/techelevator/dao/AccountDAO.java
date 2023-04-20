6a49ef0894793e1ed352560c3e19d1d236ba148a
Laura Martinez <lmmtz12@yahoo.com>
2023-04-20 11:27:56 -0500

    Adding updateAccount in JDBCAccount

--- backend-server/src/main/java/com/techelevator/dao/AccountDAO.java
+++ backend-server/src/main/java/com/techelevator/dao/AccountDAO.java
@@ -2,10 +2,16 @@ package com.techelevator.dao;
 
 import com.techelevator.model.Account;
 
+import java.util.List;
+
 public interface AccountDAO {
+
+    List<Account> allAccounts();
 
     Account getAccount(int account_id);
 
     Account createAccount(Account account);
+
+    Account updateAccount(Account account, int account_id);
 
 } // End of AccountDAO
--- backend-server/src/main/java/com/techelevator/dao/JDBCAccountDao.java
+++ backend-server/src/main/java/com/techelevator/dao/JDBCAccountDao.java
@@ -6,15 +6,24 @@ import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class JDBCAccountDao implements AccountDAO {
+
+    private List<Account> listOfAccounts = new ArrayList<>();
 
     private final JdbcTemplate jdbcTemplate;
 
     public JDBCAccountDao(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);}
 
+
+    @Override
+    public List<Account> allAccounts() {
+        return null;
+    }
 
     @Override
     public Account getAccount(int account_id) {
@@ -30,10 +39,30 @@ public class JDBCAccountDao implements AccountDAO {
 
     @Override
     public Account createAccount(Account account) {
-        String sql = "INSERT INTO account (email_address, genre, adult_only, popularity )" + "VALUES (?, ?, ?, ?) RETURNING account_id;";
-        Integer newAccountId = jdbcTemplate.queryForObject(sql, Integer.class, account.getEmail_address(), account.getGenre(), account.isAdult_only(), account.getPopularity());
+        String sql = "INSERT INTO account (user_id, username, email_address, genre, adult_only, popularity )" + "VALUES (?, ?, ?, ?, ?, ?) RETURNING account_id;";
+        Integer newAccountId = jdbcTemplate.queryForObject(sql, Integer.class, account.getAccount_id(), account.getUsername(), account.getEmail_address(), account.getGenre(), account.isAdult_only(), account.getPopularity());
 
         return getAccount(newAccountId);
+    }
+
+    @Override
+    public Account updateAccount(Account account, int account_id) {
+        Account updatedAccount = account;
+        boolean isUpdated = false;
+        for (int i= 0; i < listOfAccounts.size(); i++){
+            if (listOfAccounts.get(i).getAccount_id() == account_id){
+                if (updatedAccount.getAccount_id() == 0){
+                    updatedAccount.setAccount_id(account_id);
+                }
+                listOfAccounts.set(i, updatedAccount);
+                isUpdated = true;
+                break;
+            }
+        }
+        if (!isUpdated){
+            return null;
+        }
+        return updatedAccount;
     }
 
     private Account mapRowToUser(SqlRowSet rowSet){
