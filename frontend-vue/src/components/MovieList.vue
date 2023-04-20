909e74987ecf7df456c4610f3ee167e89fde2ec1
Laura Phung <sincerelylau@gmail.com>
2023-04-20 12:58:12 -0400

    barebones structure of the browse movies display

---
+++ frontend-vue/src/components/MovieList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <!--this -should- use moviedisplay with a v-for each movie json object-->
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
