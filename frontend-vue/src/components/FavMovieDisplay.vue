6b67be84f2930055ca54062a17e779521a3361db
Keith Bacote <kbacote12@gmail.com>
2023-04-25 12:05:38 -0400

    l

---
+++ frontend-vue/src/components/FavMovieDisplay.vue
@@ -0,0 +1,28 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import DatabaseService from '../services/DatabaseService';
+export default {
+
+
+
+methods: {
+    getFavoriteMovie() {
+      const favMovie = {
+         account_id: this.$store.state.account.account_id,
+          movie_id: this.movie.id,
+          title: this.movie.original_title,
+      };
+    DatabaseService.getFavorite(favMovie)
+    }
+
+},
+}
+     
+
+</script>
+
+<style>
+</style>
---
+++ frontend-vue/src/components/FavMovieList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <h1> Browse Favorite Movies</h1>
+
+     <!-- <movie-display v-for="favoritedMovie in this.$store.state.currentMovieList.results" v-bind:key="singlemovie.id"
+                     v-bind:movie = "favorited"/-->
+
+  </div>
+</template>
+
+<script>
+//import FavMovieDisplay from '../components/FavMovieDisplay.vue'; 
+
+
+
+export default {
+    name:"fav-movie-list",
+    components:{
+        //FavMovieDisplay
+    },
+}
+
+
+</script>
+
+<style>
+
+</style>
