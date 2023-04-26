40b6053adba8fac5afe0b85613e8d0d60024e17b
Marcus Patterson <mpatterson8oh@gmail.com>
2023-04-26 17:48:56 -0400

    restart

--- frontend-vue/src/components/FavMovieDisplay.vue
+++ frontend-vue/src/components/FavMovieDisplay.vue
@@ -2,7 +2,7 @@
   <!--this should show ONE movie with the option of liking or disliking, have you watched it?-->
   <div class="display">
     <!--uses props to get the data from the API service -->
-    <div>
+    <div class = "table">
        <button class="button" v-on:click="removeFavorite">Remove from Favorites</button>
       <!--trying to display the movie title and release date-->
       <h4 class="favmovie-title">{{ movie.original_title }}</h4>
@@ -10,13 +10,14 @@
       <!-- tries to display the poster image. it's always the v-bind:src and whatever is in the poster_path: from the json object -->
       <img
         v-bind:src="
-          'https://image.tmdb.org/t/p/original/' + favmovie.poster_path"
+          'https://image.tmdb.org/t/p/original/' + movie.poster_path"
         alt="favmovie.original_title"
       />
       <!--Trying to display overview/plot of the movie-->
-      <p>{{ favmovie.overview }}</p>
+      <div class = "transparent" >
+      <br><br><p>{{ movie.overview }}</p>
     </div>
-    
+    </div>
       <!-- delete button -->
   </div>
 </template>
@@ -72,4 +73,55 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+
+h5 {
+  font-size: 60px;
+  text-shadow: 5px 5px #19181a;
+  font-family: 'Yanone Kaffeesatz', sans-serif;
+  background-color: rgba(34, 56, 97, .8);
+  text-align: center;
+  box-shadow: rgb(19, 31, 44) 30px 30px 30px -15px;
+  padding: 10px;
+}
+h4 {
+  font-size: 80px;
+  text-shadow: 5px 5px #19181a;
+  font-family: 'Yanone Kaffeesatz', sans-serif;
+  width: 1400px;
+  background-color: rgba(34, 56, 97, .8);
+  text-align: center;
+  margin-top: -850px;
+  padding: 20px;
+  
+}
+p {
+  font-size: 80px;
+  text-shadow: 5px 5px #19181a;
+  font-family: 'Yanone Kaffeesatz', sans-serif;
+  height: 800px;
+  text-align: center;
+  padding:40px;
+  background-color: rgba(34, 56, 97, .8);
+  box-shadow: rgb(19, 31, 44) 30px 30px 30px -15px;
+  
+}  
+img {
+  height:1100px;
+  width: 800px;
+  margin-top: 40px;
+  margin-bottom: 40px;
+  background-color: rgb(34, 56, 97);
+  box-shadow: rgb(19, 31, 44) 30px 30px 30px -15px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  
+}
+button {
+  margin-top: 45px;
+  margin-left: 20px;
+  margin-bottom: -25px;
+}
+
+</style>
